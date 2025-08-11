f646599533319bcea0b0f02d514d3e6a0dc3da22
asfnwr <asfnwr@gmail.com>
2025-08-11 19:42:05 +0530

    First react template

---
+++ invoice-react/marssolution/src/App.jsx
@@ -0,0 +1,29 @@
+//import logo from './logo.svg';
+import logo from './mars_logo_v3.jpg' 
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to Mars Solutions
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://Marssolutions.in"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit for more informattion
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
