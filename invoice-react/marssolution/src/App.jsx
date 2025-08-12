addb78cfd17e08a6f60cdf1fe15d3b474adf71e0
asfnwr <asfnwr@gmail.com>
2025-08-12 06:37:35 +0530

    Updated logo and link for more information

--- invoice-react/marssolution/src/App.jsx
+++ invoice-react/marssolution/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         </p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Connect with us for web development, software automation.
         </p>
         <a
           className="App-link"
