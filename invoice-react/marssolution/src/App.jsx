7a5cc43d1e38ed3591598a5ae4ce3caa223f16db
asfnwr <asfnwr@gmail.com>
2025-08-13 06:44:08 +0530

    Front end for Task management system

--- invoice-react/marssolution/src/App.jsx
+++ invoice-react/marssolution/src/App.jsx
@@ -1,35 +1,80 @@
-//import logo from './logo.svg';
-import logo from './mars_logo_v3.jpg' 
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Card from 'react-bootstrap/Card'
+import Table from 'react-bootstrap/Table'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
-  let index = 2;
+  //let index = 2;
   return (
-    <div className="App">
-      <header className="App-header">        
-        <p>
-          Welcome to Mars Solutions
-        </p>
-        <div>
-          <p>
-            A one stop solution to your amazing Website development
-          </p>
-          <div>{index}</div>
-        </div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Connect with us for web development, software automation.
-        </p>
-        <a
-          className="App-link"
-          href="https://Marssolutions.in"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Visit for more informattion
-        </a>
-      </header>
-    </div>
+    <>
+      <br />
+      <Navbar bg="light" data-bs-theme="light" fixed="top">
+        <Container>
+          <Navbar.Brand href="#home">Maxpro Tracker</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#tasklist">Available Tasks</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Navbar bg="dark" data-bs-theme="dark" fixed="bottom">
+        <Container>
+          <Navbar.Brand href="#home">Maxpro Tracker</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#taskslist">Available Tasks</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+
+      <br />
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Action
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/Add Task">Add Task</Dropdown.Item>
+          <Dropdown.Item href="#/Edit Task">Edit Task</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <br />
+
+      <Card>
+        <Card.Body>Please add task details and submit.</Card.Body>
+      </Card>
+      <br />
+
+    <Table striped bordered hover size="sm">
+      <thead>
+        <tr>
+          <th>Test Case ID</th>
+          <th>Name</th>
+          <th>Task description</th>
+          <th>Efforts in Hrs</th>
+
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+      </tbody>
+    </Table>
+    <br />
+      <Button as="input" type="submit" value="Submit"/>
+    </>
   );
 }
 
